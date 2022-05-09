a60a508df3806f3e0d6dfb716e1d78884af163f0
Jennyfer C Santos <33470911+jennyarch@users.noreply.github.com>
2022-05-09 20:16:06 -0300

    alterado arquivo da pasta itens

--- src/pages/cardapio/Itens/index.tsx
+++ src/pages/cardapio/Itens/index.tsx
@@ -13,31 +13,34 @@ export default function Itens(props: Props) {
   const [lista, setLista] = useState(cardapio); // o estado inicial será o cardapio
   const { busca, filtro, ordenador } = props;
 
-  function testaBusca(title: string){
-    const regex = new RegExp(busca, 'i')//o serve para desconsiderar o case sensitive
-    return regex.test(title);
-  }
-
-  function testaFiltro(id: number){
-    if(filtro !== null) return filtro === id; //se existir o filtro, vamos retornar filtro igual a iD
-    return true;
-  }
-
-  function ordenar(novaLista: typeof cardapio){
-    switch(ordenador){
-      case 'porcao':
-        return novaLista.sort((a, b) => a.size > b.size ? 1 : -1)
-      case 'qtd_pessoas':
-        return novaLista.sort((a, b) => a.serving > b.serving ? 1 : -1)
-      case 'preco':
-        return novaLista.sort((a, b) => a.price > b.price ? 1 : -1)
-      default:
-        return novaLista;
-    }
-  }
+  
   useEffect(() => {
+    function testaBusca(title: string){
+      const regex = new RegExp(busca, 'i')//o serve para desconsiderar o case sensitive
+      return regex.test(title);
+    }
+  
+    function testaFiltro(id: number){
+      if(filtro !== null) return filtro === id; //se existir o filtro, vamos retornar filtro igual a iD
+      return true;
+    }
+  
+    function ordenar(novaLista: typeof cardapio){
+      switch(ordenador){
+        case 'porcao':
+          return novaLista.sort((a, b) => a.size > b.size ? 1 : -1)
+        case 'qtd_pessoas':
+          return novaLista.sort((a, b) => a.serving > b.serving ? 1 : -1)
+        case 'preco':
+          return novaLista.sort((a, b) => a.price > b.price ? 1 : -1)
+        default:
+          return novaLista;
+      }
+    }
+
     const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
     setLista(ordenar(novaLista));
+    
   },[busca, filtro, ordenador])// sempre que esses atualizarem o useeffect irá rodar
 
   return (
